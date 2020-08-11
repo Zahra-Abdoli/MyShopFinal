91c28badb34eb4e2c955eef65b9a9ce9e3f6b371
zahra abdoli <abdoli_zahra2@yahoo.com>
2020-08-11 03:30:42 -1000

    productRepository completed

--- MyShop/Myshop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/Myshop.WebUI/Controllers/ProductManagerController.cs
@@ -5,16 +5,22 @@ using System.Web;
 using System.Web.Mvc;
 using MyShop.Core.Models;
 using MyShop.DataAccess.InMemory;
+using MyShop.Core.ViewModels;
+
 
 namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
         ProductRepository context;
+        ProductCategoryRepository productCategories;
+
 
         public ProductManagerController()
         {
             context = new ProductRepository();
+            productCategories = new ProductCategoryRepository();
+
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -24,8 +30,11 @@ namespace MyShop.WebUI.Controllers
         }
         public ActionResult Create()
         {
-            Product product = new Product();
-            return View(product);
+            ProductManagerViewModel viewModel = new ProductManagerViewModel();
+            viewModel.Product = new Product();
+            viewModel.ProductCategories = productCategories.Collection();
+            return View(viewModel);
+
         }
         [HttpPost]
         public ActionResult Create(Product product)
@@ -36,9 +45,10 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
-                context.Insert(product);
-                context.Commit();
-                return RedirectToAction("Index");
+                ProductManagerViewModel viewModel = new ProductManagerViewModel();
+                viewModel.Product = product;
+                viewModel.ProductCategories = productCategories.Collection();
+                return View(viewModel);
 
             }
         }
